849e52ed33165cb2bfbd276177b245606fa3d732
André Carvalho <andreunix@proton.me>
2026-06-08 21:27:56 -0400

    chore: configure fiscal package entrypoint

---
+++ src/fiscal/index.ts
@@ -0,0 +1,7 @@
+export * from "../fiscal-core/index.ts";
+export * from "../fiscal-crypto/index.ts";
+export * from "../fiscal-cte/index.ts";
+export * from "../fiscal-mdfe/index.ts";
+export * from "../fiscal-nfse-mun/index.ts";
+export * from "../fiscal-xsd/index.ts";
+export * from "../fiscal-sefaz/index.ts";
